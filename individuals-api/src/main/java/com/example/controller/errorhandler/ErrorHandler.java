84c979a3fdabe7c167b47e46c684722a884c70f1
Uladzislau.Kaliaha <uladzislau.kaliaha@noogadev.com>
2025-07-06 01:29:21 +0300

    added README.md

--- individuals-api/src/main/java/com/example/controller/errorhandler/ErrorHandler.java
+++ individuals-api/src/main/java/com/example/controller/errorhandler/ErrorHandler.java
@@ -1,28 +1,33 @@
 package com.example.controller.errorhandler;
 
+import reactor.core.publisher.Mono;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.exception.IndividualException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ErrorHandler {
 
     @ExceptionHandler(IndividualException.class)
-    public ResponseEntity<?> handleIndividualException(Exception ex) {
-        return ResponseEntity
-            .status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body(Map.of("message", ex.getMessage()));
+    public Mono<ResponseEntity<Map<String, String>>> handleIndividualException(IndividualException ex) {
+        return Mono.just(
+            ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("message", ex.getMessage()))
+        );
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> handleGenericException(Exception ex) {
-        return ResponseEntity
-            .status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body(Map.of("message", "An unexpected error occurred"));
+    public Mono<ResponseEntity<Map<String, String>>> handleGenericException(Exception ex) {
+        return Mono.just(
+            ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of("message", "An unexpected error occurred"))
+        );
     }
 }
