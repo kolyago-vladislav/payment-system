e8ca442c2dbd9b871909c72feb67f23c41def89d
Uladzislau.Kaliaha <uladzislau.kaliaha@noogadev.com>
2025-07-10 20:18:12 +0300

    working generation api logic

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -25,42 +25,90 @@ dependencies {
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocOpenapiStarterWebmvcUiVersion")
 	implementation("javax.validation:validation-api:$javaxValidationApiVersion")
     implementation("javax.annotation:javax.annotation-api:$javaxAnnotationApiVersion")
-}
 
-tasks.register("generateAllOpenApi") {
-    openApiDir
-        .listFiles { file -> file.extension in listOf("yaml", "yml") }
-        ?.forEach { specFile ->
-            val nameWithoutExt = specFile.nameWithoutExtension.replace(Regex("[^A-Za-z0-9]"), "").capitalize()
-            dependsOn("generate${nameWithoutExt}Api")
-        }
+    compileOnly("com.google.code.findbugs:jsr305:3.0.2")
 }
 
 val openApiDir = file("$rootDir/openapi")
 
-openApiDir
-    .listFiles { file -> file.extension in listOf("yaml", "yml") }
-    ?.forEach { specFile ->
-        val nameWithoutExt = specFile.nameWithoutExtension.replace(Regex("[^A-Za-z0-9]"), "").capitalize()
-        val taskName = "generate${nameWithoutExt}Api"
+val foundSpecifications = openApiDir.listFiles { f -> f.extension in listOf("yaml", "yml") } ?: emptyArray()
+logger.lifecycle("Found ${foundSpecifications.size} specifications: " + foundSpecifications.joinToString { it.name })
 
-        tasks.register(taskName, GenerateTask::class) {
-            generatorName.set("spring")
-            inputSpec.set(specFile.absolutePath)
-            outputDir.set("$buildDir/generated-sources/openapi/${specFile.nameWithoutExtension}")
-            apiPackage.set("com.example.${specFile.nameWithoutExtension}.api")
-            modelPackage.set("com.example.${specFile.nameWithoutExtension}.dto")
-            configOptions.set(
-                mapOf(
-                    "interfaceOnly" to "true",
-                    "library" to "spring-cloud",
-                    "skipDefaultInterface" to "true",
-                    "useBeanValidation" to "true",
-                    "openApiNullable" to "false"
-                )
+fun buildTaskName(name: String): String {
+    val prepareName = name
+        .split(Regex("[^A-Za-z0-9]"))
+        .filter { it.isNotBlank() }
+        .joinToString("") { it.replaceFirstChar(Char::uppercase) }
+
+    return "generate${prepareName}"
+}
+
+fun defineJavaPackageName(name: String): String {
+     val beforeDash = name.substringBefore('-')
+     val match = Regex("^[a-z]+").find(beforeDash)
+     return match?.value ?: beforeDash.lowercase()
+}
+
+fun getAbsolutePath(nameWithoutExtension: String): Provider<String> {
+    return layout.buildDirectory
+            .dir("generated-sources/openapi/${nameWithoutExtension}")
+            .map { it.asFile.absolutePath }
+}
+
+foundSpecifications.forEach { specFile ->
+    val taskName = buildTaskName(specFile.nameWithoutExtension)
+    logger.lifecycle("taskName to be registered " + taskName)
+
+    val outDir = getAbsolutePath(specFile.nameWithoutExtension)
+    logger.lifecycle("Register task $taskName from $outDir")
+
+    val packageName = defineJavaPackageName(specFile.nameWithoutExtension)
+    logger.lifecycle("packageName " + packageName)
+
+    tasks.register(taskName, GenerateTask::class) {
+        generatorName.set("spring")
+        inputSpec.set(specFile.absolutePath)
+        outputDir.set(outDir)
+        apiPackage.set("com.example.${packageName}.api")
+        modelPackage.set("com.example.${packageName}.dto")
+
+        configOptions.set(
+            mapOf(
+                "interfaceOnly"        to "true",
+                "library"              to "spring-cloud",
+                "skipDefaultInterface" to "true",
+                "useBeanValidation"    to "true",
+                "openApiNullable"      to "false"
             )
+        )
+
+        doFirst {
+            logger.lifecycle("$taskName: starting generation from ${specFile.name}")
         }
     }
+}
+
+val withoutExtensionNames = foundSpecifications
+    ?.map { it.nameWithoutExtension } ?: emptyList()
+
+sourceSets.named("main") {
+    withoutExtensionNames.forEach { name ->
+        java.srcDir(layout.buildDirectory.dir("generated-sources/openapi/$name/src/main/java"))
+    }
+}
+
+tasks.register("generateAllOpenApi") {
+    foundSpecifications.forEach { specFile ->
+        dependsOn(buildTaskName(specFile.nameWithoutExtension))
+    }
+    doLast {
+        logger.lifecycle("generateAllOpenApi: all specifications has been generated")
+    }
+}
+
+tasks.named("compileJava") {
+    dependsOn("generateAllOpenApi")
+}
 
 publishing {
     publications {
@@ -80,14 +128,6 @@ publishing {
                 username = "admin"
                 password = "admin"
             }
-        }
-    }
-}
-
-sourceSets {
-    main {
-        java {
-            srcDir("$buildDir/generated-sources/openapi/src/main/java")
         }
     }
 }
