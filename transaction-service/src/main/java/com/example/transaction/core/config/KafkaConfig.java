c700ad15a8730bb5d1963bbea375fbc730ea4433
Uladzislau.Kaliaha <uladzislau.kaliaha@noogadev.com>
2025-07-24 23:47:02 +0300

    kafka config refactored

--- transaction-service/src/main/java/com/example/transaction/core/config/KafkaConfig.java
+++ transaction-service/src/main/java/com/example/transaction/core/config/KafkaConfig.java
@@ -4,7 +4,6 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +13,6 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
@@ -24,49 +22,38 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Configuration
 public class KafkaConfig {
 
-
-    @Bean
-    public JsonSerializer<Object> jsonSerializer(ObjectMapper objectMapper) {
-        var serializer = new JsonSerializer<>(objectMapper);
-        serializer.setAddTypeInfo(false);
-        return serializer;
-    }
-
-    @Bean
-    public ProducerFactory<String, Object> producerFactory(
-        KafkaProperties kafkaProperties,
-        JsonSerializer<Object> jsonSerializer
-    ) {
-        return new DefaultKafkaProducerFactory<>(kafkaProperties.buildProducerProperties(), new StringSerializer(), jsonSerializer);
-    }
-
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate(
-        ProducerFactory<String, Object> producerFactory
+        KafkaProperties kafkaProperties,
+        ObjectMapper objectMapper
     ) {
+        var producerProps = kafkaProperties.buildProducerProperties();
+        var producerFactory = new DefaultKafkaProducerFactory<>(
+            producerProps,
+            new StringSerializer(),
+            new JsonSerializer<>(objectMapper)
+        );
         return new KafkaTemplate<>(producerFactory);
     }
 
     @Bean
-    public ConsumerFactory<Object, Object> transactionConsumerFactory(KafkaProperties kafkaProperties) {
-        var properties = kafkaProperties.buildConsumerProperties();
+    public ConsumerFactory<Object, Object> consumerFactory(KafkaProperties kafkaProperties) {
+        var props = kafkaProperties.buildConsumerProperties();
 
-        properties.remove(JsonDeserializer.VALUE_DEFAULT_TYPE);
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
-        properties.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, ByteArrayDeserializer.class);
+        props.remove(JsonDeserializer.VALUE_DEFAULT_TYPE);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, ByteArrayDeserializer.class);
 
-        return new DefaultKafkaConsumerFactory<>(properties);
+        return new DefaultKafkaConsumerFactory<>(props);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<Object, Object> kafkaListenerContainerFactory(
-        @Qualifier("transactionConsumerFactory")
-        ConsumerFactory<Object, Object> transactionConsumerFactory,
+        ConsumerFactory<Object, Object> consumerFactory,
         ConcurrentKafkaListenerContainerFactoryConfigurer configurer
     ) {
         var factory = new ConcurrentKafkaListenerContainerFactory<>();
-        configurer.configure(factory, transactionConsumerFactory);
+        configurer.configure(factory, consumerFactory);
         return factory;
     }
-
 }
