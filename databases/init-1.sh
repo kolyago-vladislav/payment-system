1e485d60b5d1041398161ec52cb2d57481899038
Uladzislau.Kaliaha <uladzislau.kaliaha@noogadev.com>
2025-07-29 00:28:12 +0300

    split into different containers

--- databases/init-1.sh
+++ databases/init-1.sh
@@ -2,8 +2,8 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-SELECT 'CREATE DATABASE person'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'person')\gexec
+SELECT 'CREATE DATABASE transaction_shard_1'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'transaction_shard_1')\gexec
 EOSQL
 
 echo -e "wal_level = 'logical'" >> $PGDATA/postgresql.conf
